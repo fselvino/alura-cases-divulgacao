dae914721fdf2aca3db2f24c8b8a00ab39560c4f
fselvino <fernandoselvino@gmail.com>
2023-09-23 17:46:19 -0300

    Inicio do projeto

---
+++ pages/404.js
@@ -0,0 +1,9 @@
+import Link from "../src/components/Link"
+export default function Page404() {
+  return (
+    <div>
+      <h1>Você se perdeu e caiu na página 404 :0</h1>
+      <Link href="/" passHref>Ir para pagina Home</Link>
+    </div>
+  )
+}
---
+++ pages/faq.js
@@ -0,0 +1,9 @@
+import Link from "../src/components/Link"
+export default function FaqPage() {
+  return (
+    <div>
+      <h1>Alura Case - Pagina de pergunta FAQ</h1>
+      <Link href="/" passHref>Ir para pagina Home</Link>
+    </div>
+  )
+}
